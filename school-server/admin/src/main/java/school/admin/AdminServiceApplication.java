3e86b9bbb65239eea9ddbf4c8b91674e765ec8dd
radheshyam.jadhav <radhejadhav21@gmail.com>
2023-01-28 21:22:39 +0530

    updated

--- school-server/admin/src/main/java/school/admin/AdminServiceApplication.java
+++ school-server/admin/src/main/java/school/admin/AdminServiceApplication.java
@@ -1,28 +1,11 @@
 package school.admin;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import school.admin.dao.DeptRepository;
-import school.admin.dao.RoleRepository;
-import school.admin.entities.Department;
-import school.admin.entities.Role;
-import school.admin.entities.RolesEnum;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
 public class AdminServiceApplication {
-
 	public static void main(String[] args) {
-//		new SpringApplicationBuilder(AdminServiceApplication.class)
-//				.profiles("dev").run(args);
 		SpringApplication.run(AdminServiceApplication.class, args);
 	}
-
 }
